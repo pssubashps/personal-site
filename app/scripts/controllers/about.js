ff50595f5f0513dc2a870c27f6beef50e4ac163d
Subash PS <sp00345364@techmahindra.com>
2016-11-14 08:58:41 +0530

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name personalDemoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the personalDemoApp
+ */
+angular.module('personalDemoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name personalDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the personalDemoApp
+ */
+angular.module('personalDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
